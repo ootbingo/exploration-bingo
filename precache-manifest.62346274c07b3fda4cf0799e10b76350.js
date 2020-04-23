23c50273c20f7e9c545ee00f252ca4dac5096e23
xwmtp <xwillmtp@gmail.com>
2020-04-23 18:44:42 +0200

    Updates

---
+++ precache-manifest.62346274c07b3fda4cf0799e10b76350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc2cd7585c921d1ea0114fa8e8d1843",
+    "url": "/Path-ingo/index.html"
+  },
+  {
+    "revision": "43bdd5043515c839c6f8",
+    "url": "/Path-ingo/static/css/main.0b4d2789.chunk.css"
+  },
+  {
+    "revision": "fd891b8d4d257dcf0bd5",
+    "url": "/Path-ingo/static/js/2.b1b4a4ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Path-ingo/static/js/2.b1b4a4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43bdd5043515c839c6f8",
+    "url": "/Path-ingo/static/js/main.bec568ab.chunk.js"
+  },
+  {
+    "revision": "ca1ea6532d5f5d4904b2",
+    "url": "/Path-ingo/static/js/runtime-main.ec51965c.js"
+  }
+]);
