fda19c6f1df8e71b547531149df19bc3b50cbbef
xwmtp <xwillmtp@gmail.com>
2022-05-14 13:46:11 +0200

    Use bingo lists package

--- src/components/BingoCard.tsx
+++ src/components/BingoCard.tsx
@@ -1,46 +1,50 @@
-import React, {useState} from "react"
-import BingoBoard from "./BingoBoard"
-import {getGoals} from "../generator/getGoals"
+import React, { useState } from "react";
+import BingoBoard from "./BingoBoard";
+import { getGoals } from "../lib/getGoals";
 import BingoInfo from "./BingoInfo";
-import {DEFAULT_VERSION, isBingoVersion} from "../generator/bingoVersions";
-import {toExplorationMode, toUrlExplorationMode} from "../generator/explorationModes";
+import { isBingoVersion, latestBingoVersion } from "oot-bingo-lists";
+import { toExplorationMode, toUrlExplorationMode } from "../lib/explorationModes";
 
-function BingoCard() {
-    const [goalsCompleted, setGoalsCompleted] = useState(0);
-    const onGreen = () => setGoalsCompleted(goalsCompleted + 1)
-    const onRed = () => setGoalsCompleted(goalsCompleted - 1)
+const urlParams = new URLSearchParams(window.location.search);
 
-    const urlParams = new URLSearchParams(window.location.search)
+const urlSeed = urlParams.get("seed");
+const seed = parseInt(urlSeed || "") || Math.floor(Math.random() * 999999);
 
-    const urlSeed = urlParams.get("seed");
-    const seed = parseInt(urlSeed || '') || Math.floor(Math.random() * 999999)
+const urlVersion = urlParams.get("version");
+const version = isBingoVersion(urlVersion) ? urlVersion : latestBingoVersion;
 
-    const urlVersion = urlParams.get("version");
-    const version = isBingoVersion(urlVersion) ? urlVersion : DEFAULT_VERSION;
+const urlMode = urlParams.get("mode");
+const mode = toExplorationMode(urlMode);
 
-    const urlMode = urlParams.get("mode");
-    const mode = toExplorationMode(urlMode);
-
-    if (seed.toString() !== urlSeed || version !== urlVersion || toUrlExplorationMode(mode) !== urlMode) {
-        urlParams.set('version', version);
-        urlParams.set('mode', toUrlExplorationMode(mode));
-        urlParams.set('seed', seed.toString());
-        window.location.search = urlParams.toString();
-    }
-
-    const explorationSeed = seed + 1765913
-    const goals = getGoals(mode, explorationSeed, version);
-
-    if (seed === -1) {
-        return <></>
-    }
-
-    return (
-        <>
-            <BingoBoard goals={goals} seed={seed} onGreen={onGreen} onRed={onRed}/>
-            <BingoInfo seed={seed} version={version} goalsCompleted={goalsCompleted}/>
-        </>
-    )
+if (
+  seed.toString() !== urlSeed ||
+  version !== urlVersion ||
+  toUrlExplorationMode(mode) !== urlMode
+) {
+  urlParams.set("version", version);
+  urlParams.set("mode", toUrlExplorationMode(mode));
+  urlParams.set("seed", seed.toString());
+  window.location.search = urlParams.toString();
 }
 
-export default BingoCard
+const explorationSeed = seed + 1765913;
+const goals = getGoals(mode, explorationSeed, version);
+
+function BingoCard() {
+  const [goalsCompleted, setGoalsCompleted] = useState(0);
+  const onGreen = () => setGoalsCompleted(goalsCompleted + 1);
+  const onRed = () => setGoalsCompleted(goalsCompleted - 1);
+
+  if (seed === -1) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <BingoBoard goals={goals} seed={seed} onGreen={onGreen} onRed={onRed} />
+      <BingoInfo seed={seed} version={version} goalsCompleted={goalsCompleted} />
+    </>
+  );
+}
+
+export default BingoCard;
--- src/components/BingoInfo.tsx
+++ src/components/BingoInfo.tsx
@@ -1,22 +1,28 @@
-import React from "react"
-import {BingoVersion} from "../generator/bingoVersions";
+import React from "react";
+import { BingoVersion } from "oot-bingo-lists";
 
 interface BingoInfoProps {
-    seed: number;
-    version: BingoVersion;
-    goalsCompleted: number;
+  seed: number;
+  version: BingoVersion;
+  goalsCompleted: number;
 }
 
 function BingoInfo(props: BingoInfoProps) {
-    return (
-        <div id="results">
-            <div id="cardInfo">
-                <p>Version: <strong>{props.version}</strong></p>
-                <p id="seed">Seed: <strong>{props.seed}</strong></p>
-            </div>
-            <p>Completed goals: <strong>{props.goalsCompleted}</strong></p>
-        </div>
-    )
+  return (
+    <div id="results">
+      <div id="cardInfo">
+        <p>
+          Version: <strong>{props.version}</strong>
+        </p>
+        <p id="seed">
+          Seed: <strong>{props.seed}</strong>
+        </p>
+      </div>
+      <p>
+        Completed goals: <strong>{props.goalsCompleted}</strong>
+      </p>
+    </div>
+  );
 }
 
-export default BingoInfo
+export default BingoInfo;
